a2c2c9b3905159fe5b0efd876539949cc81f51a1
Doudou <edouard.lavaud@gmail.com>
2018-01-15 00:47:34 +0200

    Startup script to set directory structure

--- scripts/set.sh
+++ scripts/set.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Script to set up the file structure and the cron job
 
+# File structure
+mkdir -p "/backups/L1"
+mkdir -p "/backups/L2"
+mkdir -p "/backups/L3"
+
+# Prepare cron jobs
 if [ "${CRON_TIME_L1}" ]; then
   echo "${CRON_TIME_L1} /backncrypt/backup.sh 'L1'" >> temp_cron  
 fi
@@ -10,10 +17,11 @@ if [ "${CRON_TIME_L3}" ]; then
   echo "${CRON_TIME_L3} /backncrypt/backup.sh 'L3'" >> temp_cron  
 fi
 
+# Implement CRON jobs
 rm -rf /crontab.conf
 cat temp_cron > /crontab.conf
 crontab /crontab.conf
 rm temp_cron
-echo "=> Running cron task manager"
+echo "BackNcrypt => Cron task manager started"
 exec crond -f
 exit 0;
